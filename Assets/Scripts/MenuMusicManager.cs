f55ca30d0c22b829b2caa52bf592ada373b35b7c
unknown <manhndmpd09904@gmail.com>
2025-07-19 23:04:39 +0700

    menu chon map

---
+++ Assets/Scripts/MenuMusicManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuMusicManager : MonoBehaviour
+{
+    public AudioSource menuMusicSource;
+    public Slider musicSlider;
+
+    void Start()
+    {
+        // Set slider giá trị ban đầu theo âm lượng hiện tại
+        musicSlider.value = menuMusicSource.volume;
+
+        // Lắng nghe sự thay đổi giá trị slider
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+    }
+
+    void SetMusicVolume(float value)
+    {
+        menuMusicSource.volume = value;
+    }
+}
