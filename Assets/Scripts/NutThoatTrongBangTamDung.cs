fb1433b6e2f165cfbe9e2f646ecc596627165b28
unknown <manhndmpd09904@gmail.com>
2025-07-23 17:08:30 +0700

    pause game

---
+++ Assets/Scripts/NutThoatTrongBangTamDung.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NutThoatTrongBangTamDung : MonoBehaviour
+{
+      // Để sử dụng chức năng SceneManagement
+
+
+
+    // Hàm này sẽ được gọi khi button được bấm
+    public void LoadScene(string sceneName)
+    {
+        // Tải scene theo tên
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
