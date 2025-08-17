b7ae1c7dd726b759013e6c24d1636ce3c03d9a88
unknown <manhndmpd09904@gmail.com>
2025-08-17 16:07:12 +0700

    update Menu and Shop

---
+++ Assets/Scripts/LoadScene1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene1 : MonoBehaviour
+{
+    // Hàm này sẽ được gắn vào Button
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
